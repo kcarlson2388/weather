be7b5cb9dc40159b418e10c438a55c1aa2222b0b
kcarlson2388 <kristin-carlson@kellogg.northwestern.edu>
2016-01-31 16:42:59 -0600

    hw3

---
+++ weather.js
@@ -0,0 +1,26 @@
+$(function() {
+  var handleWeatherResponse = function(data) {
+    console.log(data);
+
+    // Put your code here to change the "markup" variable.
+    // Don't change any other code in this file. You will be sad.
+
+    var markup = "<p>Currently it is " + data.currently.summary + data.currently.icon " and " + data.currently.temperature + "degrees";
+
+
+
+    // End of your code
+
+    $('.weather-report').html(markup);
+  }
+  $('a.get-the-weather').on('click', function(event) {
+    event.preventDefault();
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.forecast.io/forecast/6dbe98374cc5b8f9ea63d5ec73de9c04/42.056459,-87.675267?callback=?',
+      dataType: 'jsonp',
+      contentType: "application/json",
+      success: handleWeatherResponse
+    });
+  });
+});
